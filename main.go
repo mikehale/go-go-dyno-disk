870629d3438f078debdeeacefb13ff3b371186ee
Michael Hale <mike@hales.ws>
2013-10-10 17:31:14 -0400

    init

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+import "strings"
+import "strconv"
+import "io/ioutil"
+import "time"
+
+// while true; do egrep 'xvda2|sda2' /proc/diskstats|head -1|awk '{print $14}'; sleep 1; done
+
+const (
+	// https://www.kernel.org/doc/Documentation/iostats.txt
+	DISKSTATS = "/proc/diskstats"
+)
+
+func mustParseUint64(value string) uint64 {
+	result, err := strconv.ParseUint(value, 10, 64)
+	if err != nil {
+		panic(fmt.Sprintf("Could not parse value %v as uint64", value))
+	}
+	return result
+}
+
+type diskStats struct {
+	Inflight       uint64
+	WeightedIoTime uint64
+}
+
+func parseDiskStats(rawStats string) map[string]*diskStats {
+	allStats := make(map[string]*diskStats)
+
+	for _, line := range strings.Split(rawStats, "\n") {
+		if line == "" {
+			continue
+		}
+
+		fields := strings.Fields(line)
+		name := fields[2]
+
+		if strings.HasPrefix(name, "ram") || strings.HasPrefix(name, "loop") {
+			continue
+		}
+
+		allStats[name] = &diskStats{
+			Inflight:       mustParseUint64(fields[11]),
+			WeightedIoTime: mustParseUint64(fields[13]),
+		}
+	}
+
+	return allStats
+}
+
+func count(name string, value uint64) string {
+	return fmt.Sprintf("count#%v=%v", name, value)
+}
+
+func emitStats(stats *diskStats) {
+	s := []string{
+		count("inflight", stats.Inflight),
+		count("weightediotime", stats.WeightedIoTime),
+	}
+	fmt.Println(strings.Join(s, " "))
+}
+
+func main() {
+	ticker := time.NewTicker(time.Second * 1)
+	for {
+		select {
+		case <-ticker.C:
+			data, err := ioutil.ReadFile(DISKSTATS)
+			if err != nil {
+				panic(fmt.Sprintf("Could not read %v", DISKSTATS))
+			}
+			emitStats(parseDiskStats(string(data))["sda2"])
+		}
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import "testing"
+
+const (
+	DISKSTATS_CONTENT = `
+   1       0 ram0 0 0 0 0 0 0 0 0 0 0 0
+   1       1 ram1 0 0 0 0 0 0 0 0 0 0 0
+   1       2 ram2 0 0 0 0 0 0 0 0 0 0 0
+   1       3 ram3 0 0 0 0 0 0 0 0 0 0 0
+   1       4 ram4 0 0 0 0 0 0 0 0 0 0 0
+   1       5 ram5 0 0 0 0 0 0 0 0 0 0 0
+   1       6 ram6 0 0 0 0 0 0 0 0 0 0 0
+   1       7 ram7 0 0 0 0 0 0 0 0 0 0 0
+   1       8 ram8 0 0 0 0 0 0 0 0 0 0 0
+   1       9 ram9 0 0 0 0 0 0 0 0 0 0 0
+   1      10 ram10 0 0 0 0 0 0 0 0 0 0 0
+   1      11 ram11 0 0 0 0 0 0 0 0 0 0 0
+   1      12 ram12 0 0 0 0 0 0 0 0 0 0 0
+   1      13 ram13 0 0 0 0 0 0 0 0 0 0 0
+   1      14 ram14 0 0 0 0 0 0 0 0 0 0 0
+   1      15 ram15 0 0 0 0 0 0 0 0 0 0 0
+   7       0 loop0 0 0 0 0 0 0 0 0 0 0 0
+   7       1 loop1 0 0 0 0 0 0 0 0 0 0 0
+   7       2 loop2 0 0 0 0 0 0 0 0 0 0 0
+   7       3 loop3 0 0 0 0 0 0 0 0 0 0 0
+   7       4 loop4 0 0 0 0 0 0 0 0 0 0 0
+   7       5 loop5 0 0 0 0 0 0 0 0 0 0 0
+   7       6 loop6 0 0 0 0 0 0 0 0 0 0 0
+   7       7 loop7 0 0 0 0 0 0 0 0 0 0 0
+   8       1 sda1 10697 193 345874 42090 28500 150269 1430152 157180 0 35030 199270
+   8       2 sda2 312 0 2556 1600 3543 30950 275944 750590 0 7710 752190
+ 254       0 dm-0 84 0 738 670 34481 0 275848 6068110 0 6800 6068780
+`
+)
+
+func TestEmitStats(t *testing.T) {
+	diskStats := parseDiskStats(DISKSTATS_CONTENT)["sda2"]
+	if diskStats == nil {
+		t.Fatal("no disk stats")
+	}
+	if diskStats.Inflight != 0 {
+		t.Fatal(diskStats.Inflight)
+	}
+	if diskStats.WeightedIoTime != 752190 {
+		t.Fatal(diskStats.WeightedIoTime)
+	}
+}
